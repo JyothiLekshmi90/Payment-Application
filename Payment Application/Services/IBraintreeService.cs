e05df1c869ba67479a4ed0ce9c4ae3ecf82706db
DELL <DELL@DESKTOP-Q8KLN1U>
2022-12-20 20:32:12 +0530

    Add project files.

---
+++ Payment Application/Services/IBraintreeService.cs
@@ -0,0 +1,14 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment_Application.Service
+{
+    public interface IBraintreeService
+    {
+        IBraintreeGateway CreateGateway();
+        IBraintreeGateway GetGateway();
+    }
+}
